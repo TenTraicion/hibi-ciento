264bdd683a485155671345b6789286b051a6d66a
TenTraicion <tentraicion@gmail.com>
2022-07-16 16:11:53 +0600

    D031 S11 L229

--- assets/javascript/external/app.js
+++ assets/javascript/external/app.js
@@ -1,7 +1,11 @@
 let age = 23;
 let userName = "TenTraicion";
 let hobbies = ["Sports", "Cooking", "Reading"];
-let job = {title: "Developer", place: "New York", salary: "$500k"};
+let job = {
+    title: "Developer",
+    place: "New York", 
+    salary: "$500k"
+};
 
 alert(hobbies[1]);
 alert(hobbies[0]);
