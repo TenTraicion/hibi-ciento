1c08dd8005942dc0b3439e5b1916ba8cffc3c153
TenTraicion <tentraicion@gmail.com>
2022-07-16 20:09:57 +0600

    D032 S11 L240

--- assets/javascript/external/app.js
+++ assets/javascript/external/app.js
@@ -23,16 +23,25 @@ alert("you are " + adultYears + "y/o and born in: " + born);
 // Use the function dynamically
 let fuck;
 
-function fucks(userAge) {
-    return userAge - 3;
-};
-
 // function fucks(userAge) {
-//     let result;
-//     result = userAge - 3;
-//     return result;
+//     return userAge - 3;
 // };
+
+function fucks(userAge) {
+    let result;
+    result = userAge - 3;
+    return result;
+};
 
 fuck = fucks(age);
 
 alert(age + "y/o man can fuck approx " + fuck + "y/o lady");
+
+let person = {
+    name: "tentraicion",
+    greet() {
+        alert("Hello!");
+    }
+};
+
+person.greet();
