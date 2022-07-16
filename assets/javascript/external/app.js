261df7dcf9b73b00276997cb8ac9a35c03cc72be
TenTraicion <tentraicion@gmail.com>
2022-07-16 14:15:49 +0600

    D030 S11 L227

--- assets/javascript/external/app.js
+++ assets/javascript/external/app.js
@@ -1,9 +1,7 @@
-let userAge = 23;
-let greet;
-greet = "Hi I'm TenTraicion!";
-alert(greet);
-alert(userAge);
-greet = "Megamundo is happy";
-userAge = 32;
-alert(greet);
-alert(userAge);
+let age = 23;
+let userName = "TenTraicion";
+let hobbies = ["Sports", "Cooking", "Reading"];
+
+alert(hobbies[1]);
+alert(hobbies[0]);
+alert(hobbies[2]);
