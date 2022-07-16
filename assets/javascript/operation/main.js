d97c64970736f3e1422b99ec2f7f413c10881008
TenTraicion <tentraicion@gmail.com>
2022-07-16 20:57:11 +0600

    D032 S11 L244

--- assets/javascript/operation/main.js
+++ assets/javascript/operation/main.js
@@ -6,3 +6,10 @@ console.log(10*4);
 console.log(10/4);
 console.log(10%4); // 10-(4*n) positive = 10-4*2 = 10-8 = 2
 console.log(10%3);
+
+console.log(10+3-5*10); // BODMAS and use bracket to avoid conflict
+
+let result = (10+3-5)*10;
+result = result + 1; // value will be added 1 with the old result.
+result++; //same as l13 and works with - too
+result +=5; // result=result+5 same for other cases.
