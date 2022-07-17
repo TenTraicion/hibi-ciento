2b9b3200b07e652df9b47842fa21e9f1254d8d87
TenTraicion <tentraicion@gmail.com>
2022-07-17 16:22:41 +0600

    D034 S12 L267

--- assets/javascript/advance/app.js
+++ assets/javascript/advance/app.js
@@ -6,14 +6,23 @@ console.log(window);
 console.dir(document);
 
 let uname = "tentraicion";
+let git = "https://github.com/";
+let com = "Solvinux";
 
 //drilling in DOM
 // document.body.children[2].children[0].href = "https://github.com/" + uname;
 
 // get content by id
 let anchor = document.getElementById("link");
-anchor.href = "https://github.com/" + uname;
+anchor.href = git + uname;
 
 //get content by .class, #id, htmlTaG {CSS ELEMENT/SELECTOR} first element only
 //document.querySelector("#link") = "test";
 //document.querySelectorAll("#link") = "test"; selects every element
+
+//Add element > create, access parent, insert element
+let link = document.createElement("a");
+let guini = document.querySelector("p");
+guini.append(link);
+link.href = git + com;
+link.textContent = "Company";
