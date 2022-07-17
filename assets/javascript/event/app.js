3150ef4c0691d304675da7ae82437fc9c5bc0087
TenTraicion <tentraicion@gmail.com>
2022-07-17 17:51:15 +0600

    D035 S12 L273

--- assets/javascript/event/app.js
+++ assets/javascript/event/app.js
@@ -9,3 +9,12 @@ console.log(pClicks);
 
 pClicks.addEventListener("click", fclick);
 
+//input listener
+let inp = document.querySelector("input");
+
+function retrieve() {
+    console.log(inp.value);
+}
+
+inp.addEventListener("input", retrieve);
+
