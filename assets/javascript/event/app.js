5dcdf82c25b7bbfca3841509958bc1dee245e2b3
TenTraicion <tentraicion@gmail.com>
2022-07-17 18:09:10 +0600

    D035 S12 L274

--- assets/javascript/event/app.js
+++ assets/javascript/event/app.js
@@ -12,9 +12,13 @@ pClicks.addEventListener("click", fclick);
 //input listener
 let inp = document.querySelector("input");
 
-function retrieve() {
-    console.log(inp.value);
-}
+function retrieve(event) {
+    // let val = inp.value; same as below
+    let val = event.target.value; // same as up, combined data
+    // let val = event.data; individual data
+    console.log(val);
+    // console.log(event);
+};
 
 inp.addEventListener("input", retrieve);
 
