27b6db46e3525abb8188d305e1ea9c7da3ea40bf
TenTraicion <tentraicion@gmail.com>
2022-07-17 15:28:10 +0600

    D034 S12 L262

--- assets/javascript/advance/app.js
+++ assets/javascript/advance/app.js
@@ -7,7 +7,13 @@ console.dir(document);
 
 let uname = "tentraicion";
 
+//drilling in DOM
 // document.body.children[2].children[0].href = "https://github.com/" + uname;
 
+// get content by id
 let anchor = document.getElementById("link");
 anchor.href = "https://github.com/" + uname;
+
+//get content by .class, #id, htmlTaG {CSS ELEMENT/SELECTOR} first element only
+//document.querySelector("#link") = "test";
+//document.querySelectorAll("#link") = "test"; selects every element
