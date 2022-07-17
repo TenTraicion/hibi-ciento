e9c2cf072fc2bb06ee208e98613b5a1069368e76
TenTraicion <tentraicion@gmail.com>
2022-07-17 15:22:39 +0600

    D034 S12 L261

--- assets/javascript/advance/app.js
+++ assets/javascript/advance/app.js
@@ -7,4 +7,7 @@ console.dir(document);
 
 let uname = "tentraicion";
 
-document.body.children[2].children[0].href = "https://github.com/" + uname;
+// document.body.children[2].children[0].href = "https://github.com/" + uname;
+
+let anchor = document.getElementById("link");
+anchor.href = "https://github.com/" + uname;
