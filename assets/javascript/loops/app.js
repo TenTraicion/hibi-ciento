78ceafdb2c01580c257c5634666c651dfe36b50e
TenTraicion <tentraicion@gmail.com>
2022-07-19 00:04:17 +0600

    D038 S13 L295

--- assets/javascript/loops/app.js
+++ assets/javascript/loops/app.js
@@ -7,3 +7,14 @@ const users = ["Max", "Anna", "Joel"];
 for (const user of users) {
     console.log(user);
 }
+
+const signIn = {
+    name: "TenTraicion",
+    age: 23,
+    isAdmin: true
+};
+
+for (const key in signIn) {
+    console.log(key + ": " + signIn[key]);
+    console.log(signIn[key]);
+}
