045204cf955445b2699d0fc493a8d1b7dda45c67
TenTraicion <tentraicion@gmail.com>
2022-07-18 21:06:44 +0600

    D037 S13 L285

--- assets/javascript/condional/app.js
+++ assets/javascript/condional/app.js
@@ -1,6 +1,29 @@
 const uname = "Max";
 
-if (true) {
-    console.log("Hello!");
+if (5=="5") {
+    console.log("Only compare numbers.");
 }
 
+if (5===5) {
+    console.log("check every individual content.");
+}
+
+if (5===5) {
+    console.log("check every individual content.");
+}
+
+if (4 !== 5) {
+    console.log("OUtput the false ans.");
+}
+
+if (!(5<5)) {
+    console.log("Make anything true, false");
+}
+
+if (5==5 && 3==3) {
+    console.log("Both need to be true");
+}
+
+if (5==5 || 2==3) {
+    console.log("Only one formula needs to be true.");
+}
