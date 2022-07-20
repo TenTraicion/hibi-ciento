2db85240321e9386d9a62ad3efc6e5e2d76e715f
TenTraicion <tentraicion@gmail.com>
2022-07-20 22:11:53 +0600

    implement Time

--- assets/Project/temperature/config.js
+++ assets/Project/temperature/config.js
@@ -38,7 +38,24 @@ const mo = udate.getMonth();
 const month = months[mo];
 const year = udate.getFullYear();
 const today = `${day}day, ${month} ${date}, ${year}`;
-show.innerHTML = today;
+
+const showClock = () => {
+    const time = new Date();
+    let hour = time.getHours();
+    let minute = time.getMinutes();
+    let second = time.getSeconds();
+    let meridiem = '';
+
+    hour = hour < 10 ? "0" + hour : hour;
+    minute = minute < 10 ? "0" + minute : minute;
+    second = second < 10 ? "0" + second : second;
+    meridiem = hour <= 12 ? meridiem = 'AM' : meridiem = 'PM';
+    hour = hour == 00 ? hour = 12 : hour > 12 ? hour = hour - 12 : hour;
+    let clock = `${hour} : ${minute} : ${second} ${meridiem}`;
+    show.textContent = `${today} and the Time is now: ${clock}`;
+}
+
+setInterval(showClock, 100);
 
 // c2f
 
