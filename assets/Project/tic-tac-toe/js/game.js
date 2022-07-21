569cd6679d1d10e8614e3ebd0f15589eace8cf54
Restyled.io <commits@restyled.io>
2022-07-21 18:20:35 +0000

    Restyled by clang-format

--- assets/Project/tic-tac-toe/js/game.js
+++ assets/Project/tic-tac-toe/js/game.js
@@ -2,13 +2,14 @@ function reset() {
   active = 0;
   round = 1;
   gameOver = false;
-  overG.firstElementChild.innerHTML = "You Won! <span id=\"winner\">Name</span>";
+  overG.firstElementChild.innerHTML =
+      "You Won! <span id=\"winner\">Name</span>";
   overG.style.display = "none";
 
   let field = 0;
   for (let i = 0; i < 3; i++) {
     for (let j = 0; j < 3; j++) {
-      gameData[i][j] =0;
+      gameData[i][j] = 0;
       fields[field].textContent = "";
       fields[field].classList.remove("del");
       field++;
@@ -17,26 +18,26 @@ function reset() {
 }
 
 function startGame() {
-	if (players[0].name === "" || players[1].name === "") {
-		alert("Please Set names before Starting a Game!");
-		console.log("No Player on Board!");
-		return;
-	}
+  if (players[0].name === "" || players[1].name === "") {
+    alert("Please Set names before Starting a Game!");
+    console.log("No Player on Board!");
+    return;
+  }
 
   reset();
-  
-	activePlayer.textContent = players[active].name;
-	game.style.display = "block";
+
+  activePlayer.textContent = players[active].name;
+  game.style.display = "block";
   console.log("The Game has Started! Let's Enjoy!");
 }
 
 function switchP() {
-	if (active === 0) {
-		active = 1;
-	} else {
-		active = 0;
-	}
-	activePlayer.textContent = players[active].name;
+  if (active === 0) {
+    active = 1;
+  } else {
+    active = 0;
+  }
+  activePlayer.textContent = players[active].name;
 }
 
 function sfield(event) {
@@ -44,20 +45,20 @@ function sfield(event) {
     return;
   }
 
-	const setField = event.target;
-	const setCol = setField.dataset.col - 1;
-	const setRow = setField.dataset.row - 1;
+  const setField = event.target;
+  const setCol = setField.dataset.col - 1;
+  const setRow = setField.dataset.row - 1;
 
-	if (gameData[setRow][setCol] > 0) {
-		alert("Please select an Empty Field!");
-		console.log("Invalid Selection! Try Again!");
-		return;
-	}
+  if (gameData[setRow][setCol] > 0) {
+    alert("Please select an Empty Field!");
+    console.log("Invalid Selection! Try Again!");
+    return;
+  }
 
-	setField.textContent = players[active].symbol;
-	setField.classList.add("del");
+  setField.textContent = players[active].symbol;
+  setField.classList.add("del");
 
-	gameData[setRow][setCol] = active + 1;
+  gameData[setRow][setCol] = active + 1;
 
   const win = over();
 
@@ -66,58 +67,46 @@ function sfield(event) {
     end(win);
   }
 
-	console.log(`${players[active].name} has Played!`);
+  console.log(`${players[active].name} has Played!`);
 
   round++;
-	switchP();
+  switchP();
 }
 
 function over() {
   // Check Rows
-  for (let i = 0; i < 3; i++){
-    if (
-      gameData[i][0] > 0 && 
-      gameData[i][0] === gameData[i][1] && 
-      gameData[i][0] === gameData[i][2]
-      ) {
+  for (let i = 0; i < 3; i++) {
+    if (gameData[i][0] > 0 && gameData[i][0] === gameData[i][1] &&
+        gameData[i][0] === gameData[i][2]) {
       return gameData[i][0];
     }
   }
 
   // Check Cols
-  for (let i = 0; i < 3; i++){
-    if (
-      gameData[0][i] > 0 && 
-      gameData[0][i] === gameData[1][i] && 
-      gameData[0][i] === gameData[2][i]
-      ) {
+  for (let i = 0; i < 3; i++) {
+    if (gameData[0][i] > 0 && gameData[0][i] === gameData[1][i] &&
+        gameData[0][i] === gameData[2][i]) {
       return gameData[0][i];
     }
   }
 
   // Check \
-  if (
-    gameData[0][0] > 0 && 
-    gameData[0][0] === gameData[1][1] && 
-    gameData[0][0] === gameData[2][2]
-    ) {
-      return gameData[0][0];
-    }
+  if (gameData[0][0] > 0 && gameData[0][0] === gameData[1][1] &&
+      gameData[0][0] === gameData[2][2]) {
+    return gameData[0][0];
+  }
 
-    // Check /
-  if (
-    gameData[2][0] > 0 && 
-    gameData[2][0] === gameData[1][1] && 
-    gameData[2][0] === gameData[0][2]
-    ) {
-      return gameData[2][0];
-    }
+  // Check /
+  if (gameData[2][0] > 0 && gameData[2][0] === gameData[1][1] &&
+      gameData[2][0] === gameData[0][2]) {
+    return gameData[2][0];
+  }
 
-    if (round === 9) {
-      return -1;
-    }
+  if (round === 9) {
+    return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 function end(win) {
@@ -125,11 +114,11 @@ function end(win) {
   overG.style.display = "block";
 
   if (win > 0) {
-    overG.firstElementChild.firstElementChild.textContent = players[win - 1].name + "!";
+    overG.firstElementChild.firstElementChild.textContent =
+        players[win - 1].name + "!";
     console.log(`${players[win - 1].name} has Won! Congrats!`);
   } else {
     overG.firstElementChild.textContent = "It's A Draw";
     console.log("Winner can't be Found!");
   }
-  
 }
