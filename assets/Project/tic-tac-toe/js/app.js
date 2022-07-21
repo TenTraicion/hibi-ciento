59e7c46845153ab70d651104da937ccc1b6df56b
TenTraicion <tentraicion@gmail.com>
2022-07-21 13:12:29 +0600

    D040 S14 L309

--- assets/Project/tic-tac-toe/js/app.js
+++ assets/Project/tic-tac-toe/js/app.js
@@ -1,5 +1,12 @@
+const ovrl = document.getElementById("ovrl");
+const backdrop = document.getElementById("backdrop");
+
 const ex = document.getElementById("ex");
 const ey = document.getElementById("ey");
+const cancel = document.getElementById("cancel");
 
 ex.addEventListener("click", openConfig);
 ey.addEventListener("click", openConfig);
+
+cancel.addEventListener("click", closeConfig);
+backdrop.addEventListener("click", closeConfig);
--- assets/Project/tic-tac-toe/js/config.js
+++ assets/Project/tic-tac-toe/js/config.js
@@ -1,1 +1,9 @@
-function openConfig() {}
+function openConfig() {
+  ovrl.style.display = "block";
+  backdrop.style.display = "block";
+}
+
+function closeConfig() {
+  ovrl.style.display = "none";
+  backdrop.style.display = "none";
+}
