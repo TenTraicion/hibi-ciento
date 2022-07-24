76c48ea5397350b62f7ddb2fa73dc11b63d734e1
TenTraicion <tentraicion@gmail.com>
2022-07-24 13:42:30 +0600

    Splitting functions

--- js/call.js
+++ js/call.js
@@ -10,6 +10,7 @@ const ovrl = document.getElementById("ovrl");
 const cancel = document.getElementById("exit");
 const chk = document.getElementById("chk");
 const remains = document.getElementById("count");
+const btn = document.getElementById("submit");
 
 //taking input
 const uname = document.getElementById("uname");
@@ -35,50 +36,3 @@ chk.addEventListener("click", checked);
 
 // count
 msg.addEventListener("input", update);
-
-
-//opening form
-function openConfig() {
-  ovrl.style.display = "flex";
-  backdrop.style.display = "block";
-  console.log("Form Opened!")
-}
-
-//closing form
-function closeConfig() {
-  ovrl.style.display = "none";
-  backdrop.style.display = "none";
-  console.log("Form Closed!")
-}
-
-//checkbox
-function checked() {
-  const output = `MY Information: \n Name: ${uname.value}\n Email: ${email.value}\n GitHub Profile: https://github.com/${git.value}\n Twitter: https://twitter.com/${twit.value}`;
-  const mail = `\n\n My Message:\n ${msg.value}`;
-
-  subject.value = sub.value;
-  body.value = output + mail;
-  console.log(output + mail);
-}
-
-//count
-function update(event) {
-  const entry = event.target.value;
-    
-  const count = msg.maxLength - entry.length;
-
-  remains.textContent = count;
-
-  if(count <= 10) {
-    remains.className = "error";
-    msg.classList.add("error");
-    msg.classList.remove("warning");
-  } else if(count <= 50) {
-      remains.className = "warning"
-      msg.classList.remove("error");
-      msg.classList.add("warning");
-  } else {
-      remains.className = "pos";
-      msg.classList.remove("warning");
-  }
-}
---
+++ js/config.js
@@ -0,0 +1,45 @@
+//opening form
+function openConfig() {
+  ovrl.style.display = "flex";
+  backdrop.style.display = "block";
+  console.log("Form Opened!")
+}
+
+//closing form
+function closeConfig() {
+  ovrl.style.display = "none";
+  backdrop.style.display = "none";
+  console.log("Form Closed!")
+}
+
+//checkbox
+function checked() {
+  const output = `MY Information: \n Name: ${uname.value}\n Email: ${email.value}\n GitHub Profile: https://github.com/${git.value}\n Twitter: https://twitter.com/${twit.value}`;
+  const mail = `\n\n My Message:\n ${msg.value}`;
+
+  subject.value = sub.value;
+  body.value = output + mail;
+  console.log(output + mail);
+}
+
+//count
+function update(event) {
+  const entry = event.target.value;
+    
+  const count = msg.maxLength - entry.length;
+
+  remains.textContent = count;
+
+  if(count <= 10) {
+    remains.className = "error";
+    msg.classList.add("error");
+    msg.classList.remove("warning");
+  } else if(count <= 50) {
+      remains.className = "warning"
+      msg.classList.remove("error");
+      msg.classList.add("warning");
+  } else {
+      remains.className = "pos";
+      msg.classList.remove("warning");
+  }
+}
