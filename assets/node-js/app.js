6d460418cbb07534b4435fbb05cf306b6d00857c
TenTraicion <tentraicion@gmail.com>
2022-07-29 23:08:42 +0600

    D049 S18 L351

--- assets/node-js/app.js
+++ assets/node-js/app.js
@@ -12,7 +12,7 @@ app.get("/currenttime", function(req, res) {
 });
 
 app.get("/", function(req, res) {
-  res.send("<form action=\"/store-user\" method=\"POST\"><label for=\"uname\">Your Name</label><input type=\"text\" name=\"uname\"><button>Submit</button></form>");
+  res.send("<form action=\"/store-user\" method=\"POST\"><label for=\"uname\">Your Name: </label><input type=\"text\" name=\"uname\"><button>Submit</button></form>");
 });
 
 app.post("/store-user", function(req, res) {
