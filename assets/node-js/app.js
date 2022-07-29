bfd2cc9f79a70c46c32a15eab7ef892292852085
TenTraicion <tentraicion@gmail.com>
2022-07-29 15:52:31 +0600

    D048 S18 L346

--- assets/node-js/app.js
+++ assets/node-js/app.js
@@ -1,15 +1,13 @@
-const http = require("http");
+const express = require("express");
 
-function handleRequest(request, response) {
-  if(request.url === "/currenttime") {
-    response.statusCode = 200;
-    response.end(`<h1>${new Date().toISOString()}</h1>`);
-  } else if (request.url === "/") {
-    response.statusCode = 200;
-    response.end("<h1>Hello World!</h1>");
-  }
-}
+const app = express();
 
-const server = http.createServer(handleRequest);
+app.get("/currenttime", function(req, res) {
+  res.send(`<h1>${new Date().toISOString()}</h1>`);
+});
 
-server.listen(3000);
+app.get("/", function(req, res) {
+  res.send("<h1>Hello World!</h1>");
+})
+
+app.listen(3000);
