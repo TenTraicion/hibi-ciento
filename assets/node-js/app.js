b41df0c24008b28a3bc4d8d12e57f5590b93672e
TenTraicion <tentraicion@gmail.com>
2022-07-29 19:27:34 +0600

    D048 S18 L347

--- assets/node-js/app.js
+++ assets/node-js/app.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get("/currenttime", function(req, res) {
   res.send(`<h1>${new Date().toISOString()}</h1>`);
 });
 
 app.get("/", function(req, res) {
-  res.send("<h1>Hello World!</h1>");
-})
+  res.send("<form action=\"/store-user\" method=\"POST\"><label for=\"uname\">Your Name</label><input type=\"text\" name=\"uname\"><button>Submit</button></form>");
+});
+
+app.post("/store-user", function(req, res) {
+  const uName = req.body.uname;
+  console.log(uName);
+  res.send("<h1>Username Stored!</h1>");
+});
 
 app.listen(3000);
