fc9c6977e88810a2e82d18608e32467303dd5ee2
TenTraicion <tentraicion@gmail.com>
2022-08-03 20:55:26 +0600

    D054 S21 L382

---
+++ assets/javascript/advance.js/function.js
@@ -0,0 +1,43 @@
+function greetUser(greetingPrefix = "Hello", userName = "You Peice of Trash") {
+  console.log(greetingPrefix + " " + userName +"!");
+}
+
+greetUser("Hi","user");
+greetUser();
+
+let result = 0;
+
+function sumUp(numbers) {
+
+  for(const number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+console.log(sumUp([1,2,20,12,15,50]));
+
+function sum(...numbers) {
+
+  for(const number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+console.log(sum(1,2,20,12,15,50));
+
+function sumIn(...numbers) {
+
+  for(const number of numbers) {
+    result += number;
+  }
+
+  return result;
+}
+
+const inputNumbers = [1,2,20,12,15,50];
+
+console.log(sumIn(...inputNumbers));
