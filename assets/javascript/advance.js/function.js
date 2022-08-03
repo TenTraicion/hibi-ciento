cab221caa5660f1436f548914da38e006fe70ea7
TenTraicion <tentraicion@gmail.com>
2022-08-03 21:05:34 +0600

    D054 S21 L384

--- assets/javascript/advance.js/function.js
+++ assets/javascript/advance.js/function.js
@@ -1,5 +1,5 @@
 function greetUser(greetingPrefix = "Hello", userName = "You Peice of Trash") {
-  console.log(greetingPrefix + " " + userName +"!");
+  console.log(`${greetingPrefix} ${userName}!`);
 }
 
 greetUser("Hi","user");
@@ -41,6 +41,3 @@ function sumIn(...numbers) {
 const inputNumbers = [1,2,20,12,15,50];
 
 console.log(sumIn(...inputNumbers));
-
-console.log(sumIn);
-console.dir(sumIn);
