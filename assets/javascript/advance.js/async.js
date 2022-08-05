90558f808cc5ba3ab942374046e9f8ea5dc048c7
TenTraicion <tentraicion@gmail.com>
2022-08-05 16:21:31 +0600

    D056 S21 L393

--- assets/javascript/advance.js/async.js
+++ assets/javascript/advance.js/async.js
@@ -1,9 +1,15 @@
 const fs = require("fs");
+const pr = require("fs/promises");
 
 function readFile() {
   fs.readFile("data.txt", function(error, fileData) {
     console.log("File Parsing Done!");
     console.log(fileData.toString());
+  });
+
+  pr.readFile("data.txt").then(function(fileData) {
+    console.log("Again Parsing Done!");
+    console.log(`msg: ${fileData.toString()}`);
   });
  
   console.log("Hi");
