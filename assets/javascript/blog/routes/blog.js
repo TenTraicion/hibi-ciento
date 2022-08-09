c8bf1e72d446edb40796e46792b0154efac59ed5
TenTraicion <tentraicion@gmail.com>
2022-08-09 17:37:32 +0600

    D062 S24 L430

--- assets/javascript/blog/routes/blog.js
+++ assets/javascript/blog/routes/blog.js
@@ -31,4 +31,17 @@ router.post("/posts", async function(req, res) {
   res.redirect("/posts");
 });
 
+router.get("/posts/:id", async function(req, res) {
+  const query = `
+    SELECT posts.*, authors.name AS author_name, authors.email AS author_email FROM posts
+    INNER JOIN authors ON posts.author_id = authors.id
+    WHERE posts.id = ?
+  `;
+  const [posts] = await db.query(query, [req.params.id]);
+  if(!posts || posts.length === 0) {
+    return res.status(404).render("404");
+  }
+  res.render("post-detail", {post: posts[0]});
+});
+
 module.exports = router;
