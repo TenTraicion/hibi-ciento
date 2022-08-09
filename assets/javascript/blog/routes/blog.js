ca9a8be7fb9d245f4e8eeaaa0804d044cf704a1d
TenTraicion <tentraicion@gmail.com>
2022-08-09 16:56:54 +0600

    D061 S24 L428

--- assets/javascript/blog/routes/blog.js
+++ assets/javascript/blog/routes/blog.js
@@ -17,4 +17,15 @@ router.get("/new-post", async function(req, res) {
   res.render("create-post", {authors: authors});
 });
 
+router.post("/posts", async function(req, res) {
+  const data = [
+    req.body.title,
+    req.body.summary,
+    req.body.content,
+    req.body.author
+  ]
+  await db.query("INSERT INTO posts (title, summary, body, author_id) VALUES (?)", [data]);
+  res.redirect("/posts");
+});
+
 module.exports = router;
