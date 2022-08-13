fa127568aa3f6529a57ed4706e996cc887c3ccdb
TenTraicion <tentraicion@gmail.com>
2022-08-13 22:36:39 +0600

    D065 S26 L454

--- assets/javascript/blog/mongodb/routes/blog.js
+++ assets/javascript/blog/mongodb/routes/blog.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const mongodb = require("mongodb");
 
 const db = require("../data/database");
+
+const ObjectId = mongodb.ObjectId;
 
 const router = express.Router();
 
@@ -15,6 +18,25 @@ router.get('/posts', function(req, res) {
 router.get('/new-post', async function(req, res) {
   const authors = await db.getDb().collection("authors").find().toArray();
   res.render('create-post', {authors: authors});
+});
+
+router.post("/posts", async function(req, res) {
+  const authorId = new ObjectId(req.body.author);
+  const author = await db.getDb().collection("authors").findOne({_id: authorId});
+  const newPost = {
+    title: req.body.title,
+    summary: req.body.summary,
+    body: req.body.content,
+    date: new Date(),
+    authors: {
+      id: authorId,
+      name: author.name
+    },
+  };
+
+  const result = await db.getDb().collection("posts").insertOne(newPost);
+  console.log(result);
+  res.redirect("/posts");
 });
 
 module.exports = router;
