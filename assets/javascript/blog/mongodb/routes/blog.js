7673bd7067b33f3b3350eeaa016f0b8e782cafd5
TenTraicion <tentraicion@gmail.com>
2022-08-14 21:40:44 +0600

    D066 S26 L457

--- assets/javascript/blog/mongodb/routes/blog.js
+++ assets/javascript/blog/mongodb/routes/blog.js
@@ -49,6 +49,14 @@ router.get("/posts/:id", async function(req, res) {
     return res.status(404).render("404");
   }
 
+  post.humanReadableDate = post.date.toLocaleDateString("en-Us", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+  });
+  post.date = post.date.toISOString();
+
   res.render("post-detail", {post: post, author: author});
 });
 
