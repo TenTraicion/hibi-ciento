d9c8fbd779ec44b7a53b56917e41953399980334
TenTraicion <tentraicion@gmail.com>
2022-08-19 16:59:21 +0600

    D068 S27 L467

--- assets/upload/routes/users.js
+++ assets/upload/routes/users.js
@@ -1,16 +1,18 @@
 const express = require('express');
-const multer = require("multer");
+const multer = require('multer');
+
+const db = require('../data/database');
 
 const storageConfig = multer.diskStorage({
   destination: function(req, file, cb) {
-    cb(null, "images");
+    cb(null, 'images');
   },
   filename: function(req, file, cb) {
-    cb(null, `${Date.now()}-${file.originalname}`);
+    cb(null, Date.now() + '-' + file.originalname);
   }
 });
 
-const upload = multer({storage: storageConfig});
+const upload = multer({ storage: storageConfig });
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -21,11 +23,16 @@ router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
 
-router.post("/profiles", upload.single("image"), function(res, req) {
+router.post('/profiles', upload.single('image'), async function(req, res) {
   const uploadedImageFile = req.file;
   const userData = req.body;
 
-  res.redirect("/");
+  await db.getDb().collection('users').insertOne({
+    name: userData.username,
+    imagePath: uploadedImageFile.path
+  });
+
+  res.redirect('/');
 });
 
 module.exports = router;
