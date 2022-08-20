6166f2db2feba6d7d0bd7d9baef4545b3c824b3a
TenTraicion <tentraicion@gmail.com>
2022-08-20 13:31:12 +0600

    D069 S28 L475

--- assets/javascript/ajax/routes/blog.js
+++ assets/javascript/ajax/routes/blog.js
@@ -86,7 +86,7 @@ router.get('/posts/:id/edit', async function (req, res) {
 
 router.post('/posts/:id/edit', async function (req, res) {
   const postId = new ObjectId(req.params.id);
-  const result = await db
+  await db
     .getDb()
     .collection('posts')
     .updateOne(
@@ -115,13 +115,12 @@ router.post('/posts/:id/delete', async function (req, res) {
 
 router.get('/posts/:id/comments', async function (req, res) {
   const postId = new ObjectId(req.params.id);
-  const post = await db.getDb().collection('posts').findOne({ _id: postId });
   const comments = await db
     .getDb()
     .collection('comments')
     .find({ postId: postId }).toArray();
 
-  return res.render('post-detail', { post: post, comments: comments });
+  res.json(comments);
 });
 
 router.post('/posts/:id/comments', async function (req, res) {
