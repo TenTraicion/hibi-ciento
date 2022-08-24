36fd6d37fa39abb2696c26e60a9990451cc24718
TenTraicion <tentraicion@gmail.com>
2022-08-24 14:57:29 +0600

    S071 S29 L491

--- assets/javascript/user/app.js
+++ assets/javascript/user/app.js
@@ -1,17 +1,34 @@
 const path = require('path');
 
 const express = require('express');
+const session = require("express-session");
+const mongostore = require("connect-mongodb-session");
+
+const MongoDBStore = mongostore(session);
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
 const app = express();
 
+const sessionStore = new MongoDBStore({
+  uri: "mongodb://127.0.0.1:27017",
+  databaseName: "auth-demo",
+  collection: "sessions",
+});
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  secret:"amijanina",
+  resave: false,
+  saveUninitialized: false,
+  store: sessionStore,
+}));
 
 app.use(demoRoutes);
 
