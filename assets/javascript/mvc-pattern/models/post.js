3229e772ef0cfb376842caa7a69405fe742ae6b7
TenTraicion <tentraicion@gmail.com>
2022-08-30 21:42:45 +0600

    D076 S31 L520

---
+++ assets/javascript/mvc-pattern/models/post.js
@@ -0,0 +1,18 @@
+const db = require("../data/database");
+
+class Post {
+  constructor(title, content, id) {
+    this.title = title;
+    this.content = content;
+    this.id = id;
+  }
+  async save() {
+    const result = await db.getDb().collection('posts').insertOne({
+      title: this.title,
+      content: this.content,
+    });
+    return result;
+  }
+}
+
+module.exports = Post;
--- assets/javascript/mvc-pattern/routes/blog.js
+++ assets/javascript/mvc-pattern/routes/blog.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongodb = require('mongodb');
 
 const db = require('../data/database');
+const Post = require("../models/post")
 
 const ObjectId = mongodb.ObjectId;
 const router = express.Router();
@@ -56,13 +57,8 @@ router.post('/posts', async function (req, res) {
     res.redirect('/admin');
     return; // or return res.redirect('/admin'); => Has the same effect
   }
-
-  const newPost = {
-    title: enteredTitle,
-    content: enteredContent,
-  };
-
-  await db.getDb().collection('posts').insertOne(newPost);
+  const post = new Post(enteredTitle, enteredContent);
+  await post.save();
 
   res.redirect('/admin');
 });
