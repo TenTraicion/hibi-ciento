15d0e47735f81dbdcb80b368944e51befe80e673
TenTraicion <tentraicion@gmail.com>
2023-05-12 16:11:52 +0600

    env and metadata

--- assets/Project/hosting/dynamic-site/data/database.js
+++ assets/Project/hosting/dynamic-site/data/database.js
@@ -3,9 +3,14 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 let database;
+let mongoUrl = "mongodb://127.0.0.1:27017/"
+
+if (process.env.MONGODB_URL) {
+  mongoUrl = process.env.MONGODB_URL;
+}
 
 async function initDatabase() {
-  const client = await MongoClient.connect('mongodb://127.0.0.1:27017');
+  const client = await MongoClient.connect(mongoUrl);
   database = client.db('deployment');
 }
 
