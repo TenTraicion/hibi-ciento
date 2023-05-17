85ec8eabb772eca5322dfcc4d7eae32f60c82a5e
TenTraicion <tentraicion@gmail.com>
2023-05-17 18:46:14 +0600

    Vue: D098 S36 L628

--- assets/vuejs/todos-frontend/scripts/app.js
+++ assets/vuejs/todos-frontend/scripts/app.js
@@ -2,8 +2,14 @@ const TodosApp = {
   data() {
     return {
       newTodo: 'Learn Vue.js!',
-    }
-  }
+    };
+  },
+  methods: {
+    saveTodo(event) {
+      event.preventDefault();
+      this.newTodo = 'Updated!';
+    },
+  },
 };
 
 Vue.createApp(TodosApp).mount("#todos-app");
