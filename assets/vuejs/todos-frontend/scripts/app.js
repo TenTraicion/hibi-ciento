e0e0b892362867c2be53e38e9a4f5b0a89efa215
TenTraicion <tentraicion@gmail.com>
2023-05-17 20:46:00 +0600

    Vue+API: D099 S36 L635

--- assets/vuejs/todos-frontend/scripts/app.js
+++ assets/vuejs/todos-frontend/scripts/app.js
@@ -1,6 +1,7 @@
 const TodosApp = {
   data() {
     return {
+      isLoading: false,
       todos: [],
       enteredValue: '',
       editedID: '',
@@ -69,6 +70,27 @@ const TodosApp = {
       });
     },
   },
+  async created() {
+    let response;
+    this.isLoading = true;
+    try {
+      response = await fetch('http://localhost:3000/todos');
+    } catch (error) {
+      alert('Something went wrong!');
+      this.isLoading = false;
+      return;
+   }
+
+   this.isLoading = false;
+
+    if (!response.ok) {
+     alert('Something went wrong!');
+     return;
+    }
+
+    const responseData = await response.json();
+    this.todos = responseData.todos;
+  }
 };
 
 Vue.createApp(TodosApp).mount("#todos-app");
