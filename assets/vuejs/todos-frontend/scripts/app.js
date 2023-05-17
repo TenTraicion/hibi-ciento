de88a1bb6b00ab20d61a7c78f88e429510c4828d
TenTraicion <tentraicion@gmail.com>
2023-05-17 19:23:27 +0600

    Vue: D099 S36 L632

--- assets/vuejs/todos-frontend/scripts/app.js
+++ assets/vuejs/todos-frontend/scripts/app.js
@@ -3,18 +3,39 @@ const TodosApp = {
     return {
       todos: [],
       enteredValue: '',
+      editedID: '',
     };
   },
   methods: {
     saveTodo(event) {
       event.preventDefault();
-      const newTodo = {
-        text: this.enteredValue,
-        id: new Date().toISOString(),
-      };
-      this.todos.push(newTodo);
+      if (this.editedID) {
+        const id = this.editedID;
+        const index = this.todos.findIndex(function(todo) {
+          return todo.id === id;
+        });
+        const updatedTodo = {
+          text: this.enteredValue,
+          id: this.todos[index].id,
+        };
+        this.todos[index] = updatedTodo;
+        this.editedID = '';
+      } else {
+        const newTodo = {
+          text: this.enteredValue,
+          id: new Date().toISOString(),
+        };
+        this.todos.push(newTodo);
+      }
       this.enteredValue = '';
     },
+    editTodo(id) {
+      this.editedID = id;
+      const item = this.todos.find(function(todo) {
+        return todo.id === id;
+      });
+      this.enteredValue = item.text;
+    }
   },
 };
 
